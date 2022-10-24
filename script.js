6e9f09d3f38422fe06e68cf8d7997aad8e444ae3
MR Hridu <developerhridu@gmail.com>
2022-10-25 04:19:39 +0600

    connect js to html

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+// let today3 = new Date();
+// document.body.innerHTML = "<h1>Today's date is " + today3 + "</h1>";
+
+// document.body.style.backgroundColor = "lightblue";
+console.log("Hello");
+
+let p = document.createElement("P");
+let t = document.createTextNode("Paragraph text.");
+p.appendChild(t);
+document.body.appendChild(p);
+console.log("Hello");
