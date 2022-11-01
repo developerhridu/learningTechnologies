c20f4740c742a35529d0824ce500a718652e7593
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-01 16:31:47 +0600

    error handling

--- ES6/main.js
+++ ES6/main.js
@@ -1,34 +1,63 @@
-
-
 let sum = 0;
-for (var i = 0; i <= 10; i++)
-{
-    sum += i;
+for (var i = 0; i <= 10; i++) {
+  sum += i;
 }
 console.log(sum);
 
 // Default parameter values
 function add(x = 10, y = 20) {
-
-    return x +  y;
+  return x + y;
 }
 console.log(add(4, 5));
 console.log(add()); // 30
 console.log(add(50)); // 50 + 20 = 70
 
-
 //Rest Parameter
-var sum2 =  0;
-function addArray(...num)
-{
-    for (var i = 0; i < num.length; i++)
-    {
-        sum2 += num[i];
-    }
-    console.log("sum is: ",sum2);
+var sum2 = 0;
+function addArray(...num) {
+  for (var i = 0; i < num.length; i++) {
+    sum2 += num[i];
+  }
+  console.log("sum is: ", sum2);
 }
-addArray(1,2, 3, 4, 5, 6, 7, 8, 9, 10);
-
+addArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 console.log(Array.from("ABCDEFG"));
 
+
+// For In Loop in Object Properties
+let student = { firstName: "MR", lastName: "Hridu", id: 1702153 };
+
+for(let x in student)
+{
+    console.log(student[x]);
+}
+
+
+// replace()
+let visit = "Visit Cumilla";
+console.log(visit.replace("Cumilla", "Dhaka"));
+
+// Error Handling
+function testingError(x)
+{
+    try {
+        if(x > 0 && x < 10)
+        {
+            throw "Perfect";
+        }
+        if(isNaN(x)) throw "not a number";
+        else
+        {
+            throw "Enter Number 1 - 9"
+        }
+    }
+    catch(err) {
+        console.log("input is: ", err);
+    }
+    finally {
+        console.log("Finally!");
+    }
+}
+testingError("sss");
+testingError(8);
