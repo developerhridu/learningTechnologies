5a786c47d65bf2823922d46d34555ea5a4b958d1
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-06 12:02:24 +0600

    Request Response Model

--- Node.Js/main.js
+++ Node.Js/main.js
@@ -1,10 +1,20 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer(function(req, res) {
-
-    res.end("Hello Node!");
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello, Home Page </h1>");
+    res.end();
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello, About Page </h1>");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello, Contact Page </h1>");
+    res.end();
+  }
 });
 
 server.listen(5050);
 console.log("Server Running");
-
