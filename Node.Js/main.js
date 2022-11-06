82f488a2f7e141ccb87353036b47084bb0b335dc
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-07 02:51:45 +0600

    Include Your Own Module

--- Node.Js/main.js
+++ Node.Js/main.js
@@ -1,16 +1,18 @@
 const http = require("http");
+const dt = require("./index")
 
 const server = http.createServer(function (req, res) {
   if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Hello, Home Page </h1>");
-    res.end();
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello, Home Page </h1>\n" + dt.myDateTime());
+    // res.write("Hridu")
+    res.end("Hridu");
   } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    // res.writeHead(200, { "Content-Type": "text/html" });
     res.write("<h1>Hello, About Page </h1>");
     res.end();
   } else if (req.url === "/contact") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    // res.writeHead(200, { "Content-Type": "text/html" });
     res.write("<h1>Hello, Contact Page </h1>");
     res.end();
   }
---
+++ Node.Js/spreadAndRest.js
@@ -0,0 +1,30 @@
+// let sum = 0;
+function sumOf(x, y, ...number) {
+  return x + y;
+}
+
+let num1 = [10, 20];
+// console.log(sumOf(50, 100));
+console.log(sumOf(...num1));
+// console.log(sumOf(50, 100, ...num1));
+
+const num = [10, 20, 30, 40, 50, 60, 70, 80];
+
+let maxVal = Math.max(...num);
+console.log(maxVal);
+
+// var array1 = [10, 20, 30, 40, 50];
+// var array2 = [...array1, 60, 70, 80];
+// console.log(array2);
+
+
+const arrValue = ['My', 'name', 'is', 'Jack'];
+
+arrValue.push("Juli");
+console.log(Array.isArray(arrValue));
+
+console.log(arrValue);  
+console.log(...arrValue);
+
+const val2 = [...arrValue, 'Hridu'];
+console.log(...val2);
