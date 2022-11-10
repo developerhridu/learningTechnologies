7258c2d5479305effc82e2b88be9a82084276adc
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-10 15:44:01 +0600

    express first app

---
+++ Express.js/index.js
@@ -0,0 +1,12 @@
+let express = require('express');
+
+app = express();
+
+app.get("/", function (req, res){
+    res.send("Hello Express Js!");
+});
+
+app.listen(8080, function () {
+    console.log("Server is running");
+
+})
