e432e8d5af4bfd6b443806db146d455f4bb84445
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-11 18:38:20 +0600

    callback

--- Express.js/index.js
+++ Express.js/index.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
     res.send("Hello Express Js!");
 });
 
+app.get("/contact", function (req, res) {
+    res.send("Hello Contact Page!");
+});
+
 app.listen(8080, function () {
     console.log("Server is running");
 
--- JS Syntax/callBack.js
+++ JS Syntax/callBack.js
@@ -11,3 +11,22 @@ function myAddtion(x, y, myCallback){
 }
 
 myAddtion(5, 10, myDisplay);
+
+
+//this is the callback function
+const timeoutCallback = () => {
+    console.log("Welcome!")
+  }
+  
+//   setTimeout(timeoutCallback, 3000)
+  
+function greeting(name) {
+    alert(`Hello, ${name}`);
+  }
+  
+function processUserInput(callback) {
+    const name = prompt("Please enter your name.");
+    callback(name);
+  }
+  
+processUserInput(greeting);
