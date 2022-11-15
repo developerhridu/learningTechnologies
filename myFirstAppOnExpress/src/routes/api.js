46c61be975dd7c0f85a90024d076bfed7d58d8b7
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-15 22:16:52 +0600

    my first express js api

---
+++ myFirstAppOnExpress/src/routes/api.js
@@ -0,0 +1,2 @@
+const express = require('express'); // as we will control routes by express.js so import it
+const router = express.Router();  // express.js er router module ta neya hoccee for routing
