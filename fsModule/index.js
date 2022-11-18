fce785551c690abe3058de1f0b5eb1ccb01c27bf
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-18 20:34:57 +0600

    new

--- fsModule/index.js
+++ fsModule/index.js
@@ -2,11 +2,12 @@ let fs = require('fs');
 let http = require('http');
 
 let server = http.createServer(function(req, res){
+    console.log(res);
 
     if(req.url == "/"){
-        let data =  fs.readFileSync('Home.html');
+        // let data =  fs.readFileSync('Home.html');
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
+        res.write(`<h1>Hello </h1>`);
         res.end();
         
     }
@@ -20,3 +21,4 @@ server.listen(4040, function(err){
         console.log("Server is running");
     }
 });
+         
--- myFirstAppOnExpress/index.js
+++ myFirstAppOnExpress/index.js
@@ -2,11 +2,11 @@ const app = require("./app");
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
-//
-// app.listen(process.env.RUNNING_PORT, function (){
-//     console.log("successfully "+ process.env.RUNNING_PORT);
-// })
 
-app.listen(5000, function (){
-    console.log("Server is running at "+ process.env.RUNNING_PORT);
+app.listen(process.env.RUNNING_PORT, function (){
+    console.log("successfully "+ process.env.RUNNING_PORT);
 })
+
+// app.listen(5000, function (){
+//     console.log("Server is running at "+ process.env.RUNNING_PORT);
+// })
