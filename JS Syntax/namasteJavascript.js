6ea086953426b2187a134ed9abefc612d70651c1
“[MR <mizanurrahmanhridu@gmail.com>
2022-11-24 20:53:58 +0600

    new

--- JS Syntax/namasteJavascript.js
+++ JS Syntax/namasteJavascript.js
@@ -337,9 +337,9 @@ Namaste JavaScript
 /* ---------> Callback <------------ */
 
 {
-  setTimeout(function () {
-    console.log("Timmer");
-  }, 5000)
+  // setTimeout(function () {
+  //   console.log("Timmer");
+  // }, 5000)
   function x(y) {
     console.log("x");
     y(); // y() is called
@@ -369,17 +369,54 @@ Namaste JavaScript
 */
 
 
-
+ 
 /* ---------> Asynchronous JavaScript & EVENT LOOP <------------ */
-{
 
-}
 
 /*
 1. Browser has superpowers that are lent to JS engine to execute some tasks, these superpowers include web API's such as console, location, DOM API, setTimeout, fetch, local storage.
 2. Callback functions and event handers are first stored in Web API environment and then transferred to callback queue.
 3. Promises and mutation observer are stored in API environment and then transferred to microtask queue.
-4. Event loop continuously observes call stack and when it is empty it transfers task to call stack.
+4. Event loop continuously observes call stack and when it is empty and some task on callback queue, it transfers task to call stack.
 5. Micro task is given priority over callback tasks.
-6. Too many micro tasks generated can cause Starvation (nit giving time to callback tasks to execute).
+6. Too many micro tasks generated can cause Starvation (nit giving time to callback tasks to execute) .
+7. Adding addEventListener is expensive
+8. fetch() - goes and request an api call and return a promise
+9. mircotask queue has higher priority than  callback queue.. all the task come from promises and mutasion observer will be on microtask queue
+10. Starvation of the callback queue - 
 */
+
+/* ---------> JS Engine EXPOSED <------------ */
+
+/*
+1. JS runtime environment contains all elements required to run JS program.
+2. It contains JS engine, set of API's, callback queue, microtask queue, event loop.
+3. JS engine is a piece of code.
+4. Process includes Code ---> Parsing ---> Compilation -----> Execution.
+5. Parsing breaks code into tokens and converts it into AST(Abstract Syntax Tree).
+6. Modern JS engine follows JIT compilation, it interprets while it optimises code as much as it can.
+7. Execution and Compilation are done together.
+8. Execution has Garbage collector and other optimisation such as inlining, copy elusion, inline caching etc.
+9. Mark and sweeo algorithm is used for gerbage collection
+*/
+
+/* ---------> Higher-Order Functions <------------ */
+{
+
+}
+
+/*
+First-class functions are JavaScript functions that can behave like variables. They can also be passed as arguments to higher-order functions.
+
+Higher-order functions are functions that return a function or take in a function as an argument.
+*/
+{
+  function x(){ 
+    console.log("Hello From X");
+  }
+
+  function y(x){  // y is higher order function
+    x();
+  }
+  y(x);
+}
